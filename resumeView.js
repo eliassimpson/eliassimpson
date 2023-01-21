2053051135c95029632fe25406d9c7b0aeff4a98
Elias S <elias@eps.local>
2023-01-21 11:29:31 -0600

    lightbox experiment

---
+++ resumeView.js
@@ -0,0 +1,53 @@
+const clientId = "3fccc7ec8d6c43eca6d95ca15de449bb";
+const viewerOptions = {
+    embedMode: "LIGHT_BOX",
+    defaultViewMode: "FIT_PAGE",
+    showDownloadPDF: false,
+    showPrintPDF: false
+};
+
+function fetchPDF(urlToPDF) {
+    return new Promise((resolve) => {
+        fetch(urlToPDF)
+            .then((resolve) => resolve.blob())
+            .then((blob) => {
+                resolve(blob.arrayBuffer());
+            })
+    })
+}
+
+function showPDF(urlToPDF) {
+    var adobeDCView = new AdobeDC.View({
+            clientId: clientId
+        });
+        var previewFilePromise = adobeDCView.previewFile(
+            {
+                content: { promise: fetchPDF(urlToPDF) },
+                metaData: { fileName: urlToPDF.split("/").slice(-1)[0] }
+            },
+            viewerOptions
+        );
+}
+
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    document.getElementById("showResume").addEventListener("click", function () {
+        showPDF("http://eliassimpson.com/images/online_resume_23.pdf")
+    });
+});
+
+// Add arrayBuffer if necessary i.e. Safari
+(function () {
+    if (Blob.arrayBuffer != "function") {
+        Blob.prototype.arrayBuffer = myArrayBuffer;
+    }
+
+    function myArrayBuffer() {
+        return new Promise((resolve) => {
+            let fileReader = new FileReader();
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.readAsArrayBuffer(this);
+        });
+    }
+})();
