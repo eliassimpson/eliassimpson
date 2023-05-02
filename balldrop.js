78221a09f15b5fafb4311911d49641c29ae1af7e
Elias Simpson <eliaspsimpson@maacbooc.local>
2023-05-02 13:27:05 -0500

    readability

--- balldrop.js
+++ balldrop.js
@@ -1,4 +1,4 @@
-var frameRate = 1/40;
+var frameRate = 1 / 40;
 var frameDelay = frameRate * 1000;
 var canvas = context = false;
 var loopTimer = false;
@@ -7,55 +7,53 @@ var width = document.documentElement.clientWidth;
 var allBalls = new Array();
 var bumpUp = true;
 
-var setup = function(upDown) {
-  bumpUp = upDown;
+var setup = function (bumpUpHook) {
+  bumpUp = bumpUpHook;
   if (bumpUp == false) {
     var ball = {
-      position: {x: 0, y:0},
-      velocity: {x: 0, y:0},
+      position: { x: 0, y: 0 },
+      velocity: { x: 0, y: 0 },
       radius: 0,
       restitution: -.7,
-      circle : Math.PI * 2,
+      circle: Math.PI * 2,
     };
     ballProperties(ball);
     allBalls.push(ball);
     if (allBalls.length == 1) {
       canvas = document.getElementById("canvas");
       context = canvas.getContext("2d");
-      context.canvas.width = width*1; //do we need to define? what is default?
+      context.canvas.width = width;
       context.canvas.height = height + 2;
       loopTimer = setInterval(loop, frameDelay);
     }
   }
-  //canvas.addEventListener("mousemove", onMouseMove);
 }
 
-var ballProperties = function(ball) {
-  var ran = Math.random();
-  ball.position.x = window.innerWidth * ran;
-  ran = Math.random();
-  ball.position.y = height * ran;
-  ran = Math.random();
-  if (ran * 5 > 1) {
-    if (ran * 5 > 2) {
-      if (ran * 5 > 3) {
-        if (ran * 5 > 4) {
-          ball.color = "#FF00CC";
-        }
-        else { ball.color = "#66FFFF"; }
-      }
-      else { ball.color = "#66FF66"; }
-    }
-    else { ball.color = "#FFFF33"; }
+var ballProperties = function (ball) {
+  ball.position.x = window.innerWidth * Math.random();
+  ball.position.y = height * Math.random();
+  var randomColor = Math.random();
+  if (randomColor > .2 && randomColor < .4) {
+    ball.color = "#FF00CC";
   }
-  else { ball.color = "#FF3366";}
-  ran = Math.random();
-  ball.radius = (ran*5) * (ran*8);
+  else if (randomColor >= .4 && randomColor < .6) {
+    ball.color = "#66FFFF";
+  }
+  else if (randomColor >= .6 && randomColor < .8) {
+    ball.color = "#66FF66";
+  }
+  else if (randomColor >= .8) {
+    ball.color = "#FFFF33";
+  }
+  else {
+    ball.color = "#FF3366";
+  }
+  var randomRadius = Math.random();
+  ball.radius = (randomRadius * 5) * (randomRadius * 8);
 }
 
-var loop = function() {
-
-  context.clearRect(0,0, window.innerWidth, height);
+var loop = function () {
+  context.clearRect(0, 0, window.innerWidth, height);
 
   context.save();
   for (var i = 0; i < allBalls.length; i++) {
@@ -77,7 +75,7 @@ var loop = function() {
 
 
     context.beginPath();
-    context.arc(allBalls[i].position.x, allBalls[i].position.y, allBalls[i].radius, 0, Math.PI*2, true);
+    context.arc(allBalls[i].position.x, allBalls[i].position.y, allBalls[i].radius, 0, Math.PI * 2, true);
     context.fillStyle = allBalls[i].color;
     context.fill();
     context.closePath();
